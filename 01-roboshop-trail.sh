60c258568c83ca8a79954ef070a5dc1152ee21eb
Guru <nainiguruvinder@gmail.com>
2025-10-24 12:58:25 +0530

    concepts

--- 01-roboshop-trail.sh
+++ 01-roboshop-trail.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-00dc811cc4361c4b9" # replace with your SG ID
+SG_ID="sg-09752b22deedf8220" # replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 ZONE_ID="Z08007301TGLKBZ7OY820" # replace with your ZONE ID
 DOMAIN_NAME="gurulabs.xyz" # replace with your domain
